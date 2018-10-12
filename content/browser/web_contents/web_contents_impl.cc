1b5df02e3e628e79154a2d1137334eafd45de230
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-10-12 11:35:44 -0400

    ledger fixes after cr69 rebase

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1701,9 +1701,9 @@ base::TimeTicks WebContentsImpl::GetLastHiddenTime() const {
   return last_hidden_time_;
 }
 
-void WebContentsImpl::SetLastActiveTime(base::TimeTicks last_active_time) {
+/*void WebContentsImpl::SetLastActiveTime(base::TimeTicks last_active_time) {
   last_active_time_ = last_active_time;
-}
+}*/
 
 void WebContentsImpl::WasShown() {
   UpdateVisibilityAndNotifyPageAndView(Visibility::VISIBLE);
